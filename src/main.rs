66949618527db169db2e7917e8af661860b15b1a
alt-potato <jason.why.zhuang@gmail.com>
2025-10-19 17:24:32 -0400

    Implement wildcards for search patterns

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fmt, fs};
 
+// valid pattern states
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum PatternState {
     Green,  // correct letter, correct position
@@ -18,6 +19,54 @@ impl From<char> for PatternState {
     }
 }
 
+impl fmt::Display for PatternState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PatternState::Green => write!(f, "G"),
+            PatternState::Yellow => write!(f, "Y"),
+            PatternState::Grey => write!(f, "X"),
+        }
+    }
+}
+
+// valid pattern states, with extra filters for queries
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum QueryPatternState {
+    Base(PatternState),
+    AnyValid,
+    Any,
+}
+
+impl From<char> for QueryPatternState {
+    fn from(c: char) -> Self {
+        match c.to_ascii_uppercase() {
+            '?' => QueryPatternState::AnyValid,
+            '*' => QueryPatternState::Any,
+            other => QueryPatternState::Base(PatternState::from(other)),
+        }
+    }
+}
+
+impl fmt::Display for QueryPatternState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            QueryPatternState::Base(state) => write!(f, "{}", state),
+            QueryPatternState::AnyValid => write!(f, "?"),
+            QueryPatternState::Any => write!(f, "*"),
+        }
+    }
+}
+
+impl QueryPatternState {
+    fn possible_states(&self) -> Vec<PatternState> {
+        match self {
+            QueryPatternState::Base(state) => vec![*state],
+            QueryPatternState::AnyValid => vec![PatternState::Green, PatternState::Yellow],
+            QueryPatternState::Any => vec![PatternState::Green, PatternState::Yellow, PatternState::Grey],
+        }
+    }
+}
+
 // application config
 struct Config {
     wordlist_path: String,
@@ -25,14 +74,28 @@ struct Config {
     pattern: String,
 }
 
+// two approaches:
+// 1. get pattern of every word and generate all patterns that match query pattern,
+//    then get all words that match those patterns
+//    + should use less memory, and only processes each word once (O(w))
+//    - query pattern expansion could be expensive (O(q * 3^5), or O(q * 3^l) if pattern length is variable in the future)
+// 2. for every word, check if it matches a query pattern,
+//    then collect all words that match each query pattern
+//    - words must be processed multiple times (O(wq))
+//    + simpler access after all words have been processed (O(1))
+// 
+// winner: 1 (yippee)
 fn main() {
     let config: Config = Config {
         wordlist_path: String::from("wordlist.txt"),
-        solution: String::from("amuse"),
+        solution: String::from("ideal"),
         pattern: String::from(
             r#"
-            XXXXX
-            XYXGG
+            ??*??
+            ?XXX?
+            ???X?
+            ?X?X?
+            ???X?
             GGGGG
         "#,
         ),
@@ -60,18 +123,25 @@ fn main() {
         pattern_map.entry(pattern).or_default().push(word.clone());
     }
 
-    // get solutions for each pattern we want
-    let patterns: Vec<Vec<PatternState>> = parse_pattern(&config.pattern);
+    // get solutions for each query pattern we want
+    let query_patterns: Vec<Vec<QueryPatternState>> = parse_query_pattern(&config.pattern);
     let mut possible: bool = true;
-    for pattern in patterns {
-        if let Some(solutions) = pattern_map.get(&pattern) {
+    for query in query_patterns {
+        let patterns: Vec<Vec<PatternState>> = expand_query_pattern(&query);
+        let mut solutions: Vec<&String> = Vec::new();
+
+        // for each (generated) pattern, get all words that match
+        for pattern in patterns {
+            if let Some(matches) = pattern_map.get(&pattern) {
+                // words for each pattern are guaranteed to be unique
+                solutions.extend(matches);
+            }
+        }
+
+        if solutions.len() > 0 {
             println!(
                 "Possible solutions for pattern {}:",
-                pattern
-                    .iter()
-                    .map(|s| format!("{:?}", s))
-                    .collect::<Vec<String>>()
-                    .join("")
+                query_pattern_to_string(&query)
             );
             if let Some(first_solution) = solutions.first() {
                 println!("  {}", first_solution);
@@ -82,11 +152,7 @@ fn main() {
         } else {
             println!(
                 "No possible solutions found for pattern {}.",
-                pattern
-                    .iter()
-                    .map(|s| format!("{:?}", s))
-                    .collect::<Vec<String>>()
-                    .join("")
+                query_pattern_to_string(&query)
             );
             possible = false;
         }
@@ -110,18 +176,44 @@ fn load_wordlist(path: &str, word_length: usize) -> Result<Vec<String>, std::io:
     Ok(words)
 }
 
-/// Parses a pattern string into a vector of vectors of PatternState enums.
+/// Parses a query pattern string into a vector of vectors of QueryPatternState enums.
 /// Expected format:
 ///   XXXXX
 ///   YYYYY
 ///   GGGGG
-fn parse_pattern(pattern_str: &str) -> Vec<Vec<PatternState>> {
+///   ?????
+///   *****
+fn parse_query_pattern(pattern_str: &str) -> Vec<Vec<QueryPatternState>> {
     pattern_str
         .lines()
         .map(|line| line.trim()) // trim whitespace from each line
         .filter(|line| !line.is_empty()) // filter out now-empty lines
-        .map(|line| line.chars().map(PatternState::from).collect())
+        .map(|line| line.chars().map(QueryPatternState::from).collect())
         .collect()
+}
+
+/// Expands a query pattern into a vector of vectors of PatternState enums, representing all possible patterns
+/// that match the query pattern.
+fn expand_query_pattern(pattern: &Vec<QueryPatternState>) -> Vec<Vec<PatternState>> {
+    let mut results: Vec<Vec<PatternState>> = vec![Vec::new()];
+
+    // for each query state (G, Y, X, ?, *) in query pattern...
+    for query_state in pattern {
+        let possible_states: Vec<PatternState> = query_state.possible_states();
+        
+        // ...extend results with each possible state
+        let mut new_results: Vec<Vec<PatternState>> = Vec::with_capacity(results.len() * possible_states.len());
+        for result in &results {
+            for possible_state in &possible_states {
+                let mut new_result = result.clone();
+                new_result.push(*possible_state);
+                new_results.push(new_result);
+            }
+        }
+        results = new_results;
+    }
+
+    results
 }
 
 /// Calculates the pattern for a given guess and solution.
@@ -152,4 +244,9 @@ fn calculate_pattern(guess: &str, solution: &str) -> Vec<PatternState> {
     }
 
     pattern
+}
+
+/// Converts a vector of QueryPatternState enums into a string representation, with one character per state.
+fn query_pattern_to_string(pattern: &Vec<QueryPatternState>) -> String {
+    pattern.iter().map(|s| s.to_string()).collect()
 }
